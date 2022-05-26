c7e393ab8803493711214b3e29aec758e80303e9
joaovguimaraes <vivito2@gmail.com>
2022-05-25 22:05:34 -0300

    commit da laura

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-a = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
+a = input('informe 2 numeros: ')
+print(a)
 
-def joazinho(a,size):
-     
-    maximo =a[0]
-    curr_max = a[0]
-     
-    for i in range(1,size):
-        curr_max = max(a[i], curr_max + a[i])
-        maximo = max(max_so_far,curr_max)
-         
-    return max_so_far
- 
- 
-print(joazinho(a,len(a)))
+lista = list(a)
+print(lista)
+
+index=0
+
+for x in lista:
+    
+  repeticoes = lista.count(lista[index])
+  sapo = [str(repeticoes),lista[index]]
+  print(sapo)
+  index+=1
+  sapo=[sapo[lista],]
